43ba4d7684eb3f957bdad3a901e76feb16c5987b
Guilherme Medeiros <medeirosguiufabc@gmail.com>
2025-02-12 21:14:03 -0300

    aula while

--- bootcamp_jornada/aula2_type.py
+++ bootcamp_jornada/aula2_type.py
@@ -85,26 +85,6 @@ import numpy as np
 # print(texto.upper())
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-texto = input("Escreva seu nome: ")
-print(texto.lower())
+# texto = input("Escreva seu nome: ")
+# print(texto.lower())
 
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
-
-# #### Booleanos (`bool`)
-
-# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
-
-# #### try-except e if
-
-# 21: Conversor de Temperatura
-# 22: Verificador de Palíndromo
-# 23: Calculadora Simples
-# 24: Classificador de Números
-# 25: Conversão de Tipo com Validação
- 
---
+++ bootcamp_jornada/aula3_while.py
@@ -0,0 +1,63 @@
+# Debug 
+# Permite rodar blocos de códigos ao inves de rodar toda a aplicação, visando identificar bugs 
+
+# CONTROLE DE FLUXO
+
+# IF - estrutura condicional fundamental identificando ser True or False. Ordem importa.
+# x = -3
+
+# if x < 0: 
+#     x = 0 
+#     print("Negative chenged to zero")
+# elif x == 0: 
+#     print("Zero")
+# elif x == 1: 
+#     print("Single")
+# else: 
+#     print("More")
+
+# FOR - útil para executar uma operação para cada elemento de uma coleção. Condição conhecida, sei quando parar 
+# for i in range(1,5): 
+#     print(i)
+
+# lista_de_alunos = ["Guilherme","Joao","Carlos"]
+
+# for nome in lista_de_alunos: 
+#     print(nome)
+
+# WHILE -> Condição desconhecida. Não sei por quanto tempo irá ficar rodando  
+# dados = []
+# entrada = ""
+
+# while entrada.lower() != "sair": 
+#     entrada = input("Digite um valor ou sair para terminar: ")
+#     print("Coninuar")
+
+# EXERCICIOS 
+# Você está analisando um conjunto de dados de vendas e precisa garantir 
+# que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
+# forem positivos ou "Dados inválidos" caso contrário.
+# quantidade = 3
+# preco = -20 
+
+# if quantidade >= 0 and preco >= 0: 
+#     print("Dados validos")
+# elif quantidade < 0 or preco < 0: 
+#     print("Dados Invalidos")
+
+### Exercício 11. Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+# entrada = ""
+
+# while entrada != "sair": 
+#     entrada = input("forneça palavras")
+#     print("Correto")
+
+### Exercício 12. Validação de Entrada
+# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+numero = ""
+
+while numero not in (5,10): 
+    numero = int(input("Forneça um numero: "))
+    print("Dados Inválidos")
