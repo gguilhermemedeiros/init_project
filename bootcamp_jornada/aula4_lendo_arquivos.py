d74ac235e596ac68b23adbf997c6c011b6d316c9
Guilherme Medeiros <medeirosguiufabc@gmail.com>
2025-03-04 20:39:49 -0300

    inclusao_6

--- bootcamp_jornada/aula4_lendo_arquivos.py
+++ bootcamp_jornada/aula4_lendo_arquivos.py
@@ -1,23 +1,26 @@
 # Lendo Arquivos 
 # Uma das principais funções de um engenheiro é acessar e disponibilizar 
-import os
-print(os.getcwd())
+# import os
+# print(os.getcwd())
 
-import csv
+# import csv
 
-caminho_arquivo = r"C:\Users\guilh\OneDrive\Documentos\Data Science\bootcamp_jornada\arquivo_csv.csv"
+# caminho_arquivo = r"C:\Users\guilh\OneDrive\Documentos\Data Science\bootcamp_jornada\arquivo_csv.csv"
 
-#armazenar os dados
-dados = []
+# #armazenar os dados
+# dados = []
 
-# WITH - gerenciador de contexto, abrindo e fechando os arquivos
-with open(file = caminho_arquivo, mode = "r") as arquivo: 
-    leitor_csv = csv.DictReader(arquivo)
+# # WITH - gerenciador de contexto, abrindo e fechando os arquivos
+# with open(file = caminho_arquivo, mode = "r") as arquivo: 
+#     leitor_csv = csv.DictReader(arquivo)
 
-    for linha in leitor_csv: 
-        dados.append(linha)
+#     for linha in leitor_csv: 
+#         dados.append(linha)
 
-print(dados)
+# print(dados)
+
+# FUNCOES
+# Sempre que realizarmos tarefas repetitivas e recorrentes, podemos crias funções
 
 
 
---
+++ bootcamp_jornada/aula6.py
@@ -0,0 +1,6 @@
+# PEP Style 
+# Define algumas boas práticas que é recomendado utilizar
+# Libs como o Flake8 analisam o código seguindo as boas práticas indicando as melhorias 
+# Incluimos biblioteca de pre commit que rodam esses ajustes antes de subir o commit 
+
+pip install pre-commit
