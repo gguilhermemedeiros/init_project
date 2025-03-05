5174df8ed74e227353b1d2de699c9035be8e5295
Guilherme Medeiros <medeirosguiufabc@gmail.com>
2025-03-05 19:41:50 -0300

    loguru_theory

---
+++ bootcamp_jornada/aula_9/aprof_funcao.py
@@ -0,0 +1,19 @@
+# Código criado dentro de ambiente virtual venv
+import pandas as pd 
+from loguru import logger #lib destinada a logs 
+
+# Log permite identificar os erros, bem como horário, durante o tempo que a pipeline roda 
+# Temos tipos de logs que variam a depender da criticidade do erro, indo desde um aviso até um erro: info/critical/warning 
+# logger.add("meu_app.log")
+
+# def soma(x,y): 
+#     logger.info(x)
+#     logger.info(y)
+#     logger.info(x+y)
+#     return x + y
+
+# soma(3,17) 
+
+
+ 
+
